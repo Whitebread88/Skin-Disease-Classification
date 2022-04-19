5013248a73b2c0b410710c2190b634f72efb6788
Whitebread <foongpro@gmail.com>
2022-04-20 02:53:15 +0800

    Add files via upload

--- Streamlit.py
+++ Streamlit.py
@@ -86,6 +86,7 @@ def predict(x_test, model):
     Y_pred = model.predict(x_test)
     Y_pred_classes = Y_pred.argmax(axis=1) #Convert to single digit class
     Y_prob = round(100 * (np.max(Y_pred)), 2)
+    Y_prob = Y_prob[0].tolist()
     
     return Y_prob, Y_pred_classes
 
