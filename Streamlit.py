daee51e48780c13a78d18cf5c69f0eda838b221a
Whitebread <foongpro@gmail.com>
2022-04-20 03:08:02 +0800

    Add files via upload

--- Streamlit.py
+++ Streamlit.py
@@ -78,6 +78,7 @@ def display_prediction(pred_prob):
                        1:'Poison Ivy  and other Contact Dermatitis',
                         0:'Urticaria Hives'}
     result["Classes"] = result["Classes"].map(lesion_type_dict)
+    result.class = result["Classes"]
     return result
 
 
@@ -148,7 +149,7 @@ def main():
                         x_test = data_gen_upload('test photo.jpg')
                         pred_prob, pred_class = predict(x_test, cnn_model)
                         result = display_prediction(pred_prob)
-                        predicted_class = Classes[np.argmax(predictions[0])]
+                        predicted_class = result.class[np.argmax(predictions[0])]
                         st.write("The predicted Skin Disease is: ",predicted_class)
                         st.write(result)
                         if st.checkbox('Display Probability Graph'):
